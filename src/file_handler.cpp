0df951600471b9d4459463dac5a61809e07759d6
Gabriel Sammut <eldrad294@hotmail.com>
2018-03-31 15:15:04 +0200

    Commit

--- src/file_handler.cpp
+++ src/file_handler.cpp
@@ -1,49 +1,53 @@
 #include <string>
 #include <fstream>
 #include <iostream>
-using namespace std;
 
 class FileHandler{
     public:
-        int save_to_file(string);
-        int read_from_file(string);
-};
-
-int save_to_file(string file_path)
-{
-    return 0;
-}
-
-/*
-Reads from files with name:
-
-"input_n.txt"
-
-File format eg:
-
-M,X,Y
-6.27803, -368.462, -499.992
-3.5948, 32.7673, -41.3501
-5.89648, 178.865, -452.955
-
-Returns 0 on successfull open, otherwise returns 1
-*/
-int read_from_file(string file_path)
-{
-    string line;
-    ifstream myfile (file_path);
-    if(myfile.is_open())
-    {
-        while(getline(myfile,line))
+        int save_to_file(std::string file_path)
         {
-            cout << line << "\n";
+            return 0;
         }
-        myfile.close();
-    }else
-    {
-        cout << "Unable to open file at location: [" << file_path << "]";
-        return 1;   
-    }
-    
-    return 0;
-}
+
+        /*
+        Reads from files with name:
+
+        "input_n.txt"
+
+        File format eg:
+
+        M,X,Y
+        6.27803, -368.462, -499.992
+        3.5948, 32.7673, -41.3501
+        5.89648, 178.865, -452.955
+
+        Returns 0 on successfull open, otherwise returns 1
+        */
+        int read_from_file(std::string file_path)
+        {
+            std::string line;
+            std::ifstream myfile;
+            char delimeter = ',';
+
+            /*
+            Opens input file at specified path 
+            (Note: C++11 file.open() requires an input parameter of type char) 
+            */
+            myfile.open(file_path.c_str());
+            if(myfile.is_open())
+            {
+                //Fetches content from the file line by line
+                while(getline(myfile,line))
+                {
+                    std::cout << line << "\n";
+                }
+                myfile.close();
+            }else
+            {
+                std::cout << "Unable to open file at location: [" << file_path << "]";
+                return 1;   
+            }
+            
+            return 0;
+        }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -3,26 +3,62 @@ Main class, which contains all the logic
 */
 // Library Imports
 #include <iostream>
-// External File Imports
+#include <vector>
+#include <stdlib.h>
+#include <algorithm>
 #include "file_handler.cpp"
-#include "constants.cpp"
-using namespace std;
-
+#include "vector2.h"
+/*
+Constant Declarations
+*/
+const std::string input_file_path = "/home/gabriel/HighPerformanceComputing/CPS3227_Assignment/input/input_64.txt";    
+const int fieldWidth = 1000;
+const int fieldHalfWidth = fieldWidth >> 1;
+const int fieldHeight = 1000;
+const int fieldHalfHeight = fieldHeight >> 1;
+const float minBodyMass = 2.5f;
+const float maxBodyMassVariance = 5.f;
+/*
+ * Particle structure
+ */
+struct Particle
+{
+	Vector2 Position;
+	Vector2 Velocity;
+	float	Mass;
+	
+	Particle(void) 
+		: Position( ((float)rand()) / RAND_MAX * fieldWidth - fieldHalfWidth,
+					((float)rand()) / RAND_MAX * fieldHeight - fieldHalfHeight)
+		, Velocity( 0.f, 0.f )
+		, Mass ( ((float)rand()) / RAND_MAX * maxBodyMassVariance + minBodyMass )
+	{ }
+};
+/*
+Method Definitions
+*/
+int save_to_file(std::string);
+int read_from_file(std::string);
+/*
+Main Routine
+*/
 int main(int argc, char **argv)
 {
     /*
     Variable Declarations
     */
-    Constants c;
+    /*
+    Object Declarations
+    */
     FileHandler fh;
 
     /*
     Main Logic
     */
     // Opening Input File
-    if(fh.read_from_file(c.input_file_path) != 0)
+    if(fh.read_from_file(input_file_path) != 0)
     {
-        cout << "A problem occurred during opening of the input file!";
+        std::cout << "A problem occurred during opening of the input file!";
         return 1;
     }
     return 0;
