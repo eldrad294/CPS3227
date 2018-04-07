b8e8cbfc81f116b3026a0c98ef88ca27a901c66e
Gabriel Sammut <eldrad294@hotmail.com>
2018-04-07 10:48:27 +0200

    Added Timing Function

---
+++ compiler.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Compile program
+g++ -Wall -fopenmp -std=c++11 src/*main.cpp -o build/main
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 Main class, which contains all the logic
 */
 // Library Imports
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <iostream>
 #include <sstream>
 #include "file_handler.cpp"
@@ -17,6 +20,8 @@ Method Definitions
 */
 void PersistPositions(const std::string &p_strFilename, std::vector<Particle> &p_bodies);
 std::vector<Particle> read_from_file(std::string);
+void getInfo(void);
+double captureTimestamp(void);
 /*
 Main Routine
 */
@@ -25,6 +30,8 @@ int main(int argc, char **argv)
     /*
     Variable Declarations
     */
+    double start_time_stamp = .0;
+    double end_time_stamp = .0;
     FileHandler fh;
     Particle p;
     std::stringstream fileOutput;
@@ -34,13 +41,19 @@ int main(int argc, char **argv)
     /*
     Main Logic
     */
+    getInfo(); //RLogs particular data for this specific run, concerning the system
+
     // Check if input file has been specified. Otherwise default to input_64.txt
     if(argc > 1)
     {
         input_file_path = argv[1];
     }
+    
     // Opening Input File
     bodies = fh.read_from_file(input_file_path);
+
+    // Take Initial Time Measurement
+    start_time_stamp = captureTimestamp();
 
     for (int iteration = 0; iteration < maxIteration; ++iteration)
 	{
@@ -51,5 +64,23 @@ int main(int argc, char **argv)
 		fileOutput << output_file_name << iteration << ".txt";
 		fh.PersistPositions(fileOutput.str(), bodies);
 	}
+
+    // Take Final Time Measurement
+    end_time_stamp = captureTimestamp();
+    std::cout << "Total Elapsed Time: " << end_time_stamp - start_time_stamp << " seconds\n";
     return 0;
+}
+
+double captureTimestamp(void)
+{
+    return omp_get_wtime();
+}
+
+void getInfo(void)
+{
+    std::cout << "Clock Frequency\n";
+    std::cout << "wtime = " << omp_get_wtime() << "\n";
+    std::cout << "Number of processors = " << omp_get_num_procs() << "\n";
+    std::cout << "wtick(Clock Frequency) = " << omp_get_wtick() << "\n";
+    std::cout << "1/wtick = " << 1.0/omp_get_wtick() << "\n\n";
 }
