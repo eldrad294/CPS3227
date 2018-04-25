0b53394c6ae46aede5cabe7eee34d60c084de347
Gabriel Sammut <eldrad294@hotmail.com>
2018-04-25 17:31:32 +0200

    MPI Single Run Modifications

---
+++ src/src/main.cpp
@@ -0,0 +1,135 @@
+/*
+Main class, which contains all the logic and respective references
+*/
+// Library Imports
+#include <omp.h>
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <sstream>
+#include "file_handler.cpp"
+#include "particle.cpp"
+/*
+Constant Declarations
+*/
+#define maxIteration 1000
+#define deltaT 0.01f
+#define gTerm 20.f
+/*
+Method Definitions
+*/
+void PersistPositions(const std::string, std::vector<Particle>, bool);
+std::vector<Particle> read_from_file(std::string, std::string enable_output);
+void reportInfo(std::string, double);
+void reportInfoToFile(std::string, double);
+double captureTimestamp(void);
+/*
+Main Routine
+*/
+int main(int argc, char **argv)
+{
+    /*
+    Variable Declarations
+    */
+    double start_time_stamp = .0;
+    double end_time_stamp = .0;
+    std::string enable_output = "output_off";
+    FileHandler fh;
+    Particle p;
+    std::stringstream fileOutput;
+    std::vector<Particle> bodies;
+    std::vector<Particle> local_bodies;
+    std::string input_file_path = "CPS3227_Assignment/input/input_64.txt";    
+    std::string output_file_name = "CPS3227_Assignment/output/nbody_";
+        
+    /*
+    Initialize the MPI environment
+    */
+    MPI_Init(NULL, NULL);
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size); //Get the number of processes
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank); //Get the rank of the process
+
+    /*
+    Defining MPI datatype
+    */
+    MPI_Datatype vector_obj;
+    MPI_Type_contiguous(5,MPI_FLOAT,&vector_obj);
+    MPI_Type_commit(&vector_obj);
+
+    /*
+    Main Logic
+    */
+    // Check if input file has been specified. Otherwise default to input_64.txt
+    if(argc > 1)
+    {
+        enable_output = argv[2];
+    }
+    if(argc > 0)
+    {
+        input_file_path = argv[1];
+    }
+    
+    // Read input files and start recording time from master node only
+    if (world_rank == 0)
+    {   
+        // Opening Input File
+        bodies = fh.read_from_file(input_file_path, enable_output);
+
+        // Take Initial Time Measurement
+        start_time_stamp = captureTimestamp();
+    }
+       
+    for (int iteration = 0; iteration < maxIteration; ++iteration)
+    {      
+        //Broadcast bodies for Particle Force Computation
+        MPI_Bcast(&bodies,bodies.size(),vector_obj,0,MPI_COMM_WORLD);
+       	
+        //Return sub vector of particles
+        local_bodies = p.ComputeForces(bodies, gTerm, world_rank, world_size);
+       
+        //Gather all bodies into head node
+        MPI_Gather(&local_bodies, 1, vector_obj, &bodies, 1, vector_obj, 0, MPI_COMM_WORLD);
+               
+        //Broadcast bodies for Particle Force Computation
+        MPI_Bcast(&bodies,bodies.size(),vector_obj,0,MPI_COMM_WORLD);
+                
+        //Return sub vector of particles
+	local_bodies = p.MoveBodies(bodies, deltaT, world_rank, world_size);
+        	
+        //Gather all bodies into head node
+        MPI_Gather(&local_bodies, 1, vector_obj, &bodies, 1,  vector_obj, 0, MPI_COMM_WORLD);
+        
+        if (world_rank == 0)
+        {
+	    fileOutput.str(std::string());
+            fileOutput << output_file_name << iteration << ".txt";
+            fh.PersistPositions(fileOutput.str(), bodies, enable_output);
+        }
+    }
+    
+    // Finish recording time from master node only
+    if (world_rank == 0)
+    {
+        // Take Final Time Measurement
+        end_time_stamp = captureTimestamp();
+
+        fh.reportInfo(input_file_path, end_time_stamp - start_time_stamp); //Logs increment run.
+        //fh.reportInfoToFile(input_file_path, end_time_stamp - start_time_stamp); //Logs increment run. Saves to File.
+    }
+   
+    /*
+    Destruct MPI environment
+    */
+    MPI_Type_free(&vector_obj);
+    MPI_Finalize(); //Finalize the MPI Environment
+    
+    return 0;
+}
+
+double captureTimestamp(void)
+{
+    return omp_get_wtime();
+}
---
+++ src/src/particle.cpp
@@ -0,0 +1,133 @@
+//Header Guardians
+#ifndef _MYHEADER_H_
+#define _MYHEADER_H_
+// Library Imports
+#include <algorithm>
+#include <vector>
+#include "vector2.h"
+
+class Particle
+{        
+    public:
+        Vector2 Position;
+        Vector2 Velocity;
+        float	Mass;
+
+        Particle(){}
+
+        Particle(float p_Mass, float p_X, float p_Y) 
+        { 
+            Position = Vector2(p_X, p_Y);
+            Velocity = Vector2(0.f, 0.f); // Initialize particle with velocity vector of 0
+            Mass = p_Mass;
+        }
+
+        /*
+        * Compute forces of particles exerted on one another
+        */
+        std::vector<Particle> ComputeForces(std::vector<Particle> &p_bodies, float p_gravitationalTerm, int world_rank, int 
+world_size)
+        {
+            Vector2 direction, force, acceleration;
+            float distance;
+            unsigned balanced_split, min, max;
+
+            // Calculating partition split, and current range of bodies to calculate for current node
+            balanced_split = p_bodies.size() / world_size;
+            min = balanced_split * world_rank;
+            max = min + balanced_split - 1;
+
+            // Cater for uneven partition splits
+            if(world_rank == world_size-1)
+            {
+               max = p_bodies.size();
+            }
+
+            // Creating local vector of bodies upon which to perform particle lookup
+            std::vector<Particle> local_bodies(&p_bodies[min], &p_bodies[max]);
+
+            #pragma omp parallel for default(none) private(force, acceleration) shared(local_bodies, p_bodies,p_gravitationalTerm,min)
+            for (size_t j = 0; j < local_bodies.size(); ++j)
+            {
+                Particle &p1 = p_bodies[min];
+            
+                force = 0.f, acceleration = 0.f;
+                
+                min++;   
+            
+                #pragma omp parallel for default(none) private(direction,distance,force) shared(local_bodies, p_bodies,j,min,p1)
+                for (size_t k = 0; k < p_bodies.size(); ++k)
+                {
+                    if (k == min) continue;
+                
+                    Particle &p2 = p_bodies[k];
+                    
+                    // Compute direction vector
+                    direction = p2.Position - p1.Position;
+                    
+                    // Limit distance term to avoid singularities
+                    distance = std::max<float>( 0.5f * (p2.Mass + p1.Mass), fabs(direction.Length()) );
+                    
+                    // Accumulate force
+                    force += direction / (distance * distance * distance) * p2.Mass; 
+                }
+                        
+                // Compute acceleration for body 
+                acceleration = force * p_gravitationalTerm;
+                
+                // Integrate velocity (m/s)
+                p1.Velocity += acceleration;
+            }
+            return local_bodies;
+        }
+
+        /*
+        * Update particle positions
+        */
+        std::vector<Particle> MoveBodies(std::vector<Particle> &p_bodies, float p_deltaT, int world_rank, int world_size)
+        //void MoveBodies(std::vector<Particle> p_bodies, float p_deltaT)
+        {
+            unsigned balanced_split, min, max;
+
+            // Calculating balanced split, and current range of bodies to calculate for current node
+            balanced_split = p_bodies.size() / world_size;
+            min = balanced_split * world_rank;
+            max = min + balanced_split + 1;
+
+            // Cater for uneven partition splits
+            if(world_rank == world_size-1)
+            {
+               max = p_bodies.size();
+            }
+
+            // Creating local vector of bodies upon which to perform particle lookup
+            std::vector<Particle> local_bodies(&p_bodies[min], &p_bodies[max]);
+
+            #pragma omp parallel for default(none) shared(local_bodies, p_bodies, p_deltaT)
+            for (size_t j = 0; j < local_bodies.size(); ++j)
+            {
+                local_bodies[j].Position += local_bodies[j].Velocity * p_deltaT;
+            }
+ 
+            return local_bodies;
+            // // Parallel Method
+            // std::vector<Particle> temp_list;
+            // #pragma omp parallel default(none) shared(p_bodies, p_deltaT) private(temp_list)
+            // {
+            //     #pragma omp for schedule(static)
+            //     for (size_t j = 0; j < p_bodies.size(); ++j)
+            //     {
+            //         temp_list.push_back(p_bodies[j]);
+            //         temp_list[j].Position += p_bodies[j].Velocity * p_deltaT;
+            //     }
+            //     p_bodies.clear();
+            //     #pragma omp for schedule(static) ordered //Barrier
+            //     for(int i=0; i < omp_get_num_threads(); i++)
+            //     {
+            //         #pragma omp ordered // Ensures that vectors are concatenated in correct order
+            //         p_bodies.insert(p_bodies.end(),temp_list.begin(),temp_list.end());
+            //     }
+            // }
+        }
+};
+#endif
